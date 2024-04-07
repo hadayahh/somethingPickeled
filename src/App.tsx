9c2c57bb0f908fed9cf98a410d5d098ecfa42207
hadayahh <habaza.dev@gmail.com>
2024-04-07 18:21:19 -0400

    shipping banner styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Nav } from "./Nav"
 function App() {
   return (
     <>
+      <div className="shipping-banner">
+        <div>Enjoy complimentary standard delivery and samples with all orders</div>
+      </div>
       <Nav/>
     </>
   )
