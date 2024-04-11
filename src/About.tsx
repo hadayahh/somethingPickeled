a23594f7cf0be8e8aeceb89bf5f9c93985c0768a
hadayahh <habaza.dev@gmail.com>
2024-04-10 21:45:23 -0400

    about flex containers

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,10 @@
 export function About(){
     return (
-        <div className="about">test</div>
+        <div className="about">
+            <div className="about-text">
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at ipsum. Ratione repellendus mollitia similique saepe ad praesentium voluptate doloremque, ea, asperiores voluptates necessitatibus tempora fuga dolorem amet ipsum quasi.</div>
+            </div>
+            <div className="about-image"><img src="" alt="" /></div>
+        </div>
     )
 }
