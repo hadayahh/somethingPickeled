3226b38a63b43f2574ed4b275af4c0c174dea325
hadayahh <habaza.dev@gmail.com>
2024-04-10 22:17:21 -0400

    about image flexed

--- src/About.tsx
+++ src/About.tsx
@@ -1,10 +1,12 @@
 export function About(){
     return (
         <div className="about">
-            <div className="about-text">
-                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at ipsum. Ratione repellendus mollitia similique saepe ad praesentium voluptate doloremque, ea, asperiores voluptates necessitatibus tempora fuga dolorem amet ipsum quasi.</div>
+            <div className="about-text-container">
+                <div className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at ipsum. Ratione repellendus mollitia similique saepe ad praesentium voluptate doloremque, ea, asperiores voluptates necessitatibus tempora fuga dolorem amet ipsum quasi.</div>
             </div>
-            <div className="about-image"><img src="" alt="" /></div>
+            <div className="about-image">
+                <img src="https://st5.depositphotos.com/6823598/67614/v/450/depositphotos_676149906-stock-illustration-jar-preserved-vegetables-can-pickled.jpg" alt="" />
+            </div>
         </div>
     )
 }
