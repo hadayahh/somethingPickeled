9173a3ba39bc4969f7c9d80e05bc3c4fbd5c8b29
hadayahh <habaza.dev@gmail.com>
2024-04-17 03:13:12 -0400

    footer flex template

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,12 +9,44 @@ export function Footer(){
                     <li>LinkedIn</li>
                 </ul>
             </div>
-            <div className="socials">
-                <h3>Social Media</h3>
-                <div className="socials-divider"></div>
-                <ul className="socials-list">
-                    <li>Instagram</li>
-                    <li>LinkedIn</li>
+            <div className="contact">
+                <h3>Contact us</h3>
+                <div className="contact-divider"></div>
+                <ul className="contact-list">
+                    <li>Email</li>
+                    <li>Phone Number</li>
+                </ul>
+            </div>
+            <div className="contact">
+                <h3>Contact</h3>
+                <div className="contact-divider"></div>
+                <ul className="contact-list">
+                    <li>Email</li>
+                    <li>Phone Number</li>
+                </ul>
+            </div>
+            <div className="contact">
+                <h3>Contact</h3>
+                <div className="contact-divider"></div>
+                <ul className="contact-list">
+                    <li>Email</li>
+                    <li>Phone Number</li>
+                </ul>
+            </div>
+            <div className="contact">
+                <h3>Contact</h3>
+                <div className="contact-divider"></div>
+                <ul className="contact-list">
+                    <li>Email</li>
+                    <li>Phone Number</li>
+                </ul>
+            </div>
+            <div className="contact">
+                <h3>Contact</h3>
+                <div className="contact-divider"></div>
+                <ul className="contact-list">
+                    <li>Email</li>
+                    <li>Phone Number</li>
                 </ul>
             </div>
         </div>
