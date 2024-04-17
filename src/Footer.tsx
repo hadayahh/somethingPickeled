74aa4864f9b2215be7fd9f2434496555dacbc2aa
hadayahh <habaza.dev@gmail.com>
2024-04-16 23:35:58 -0400

    responsive design

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,6 +9,14 @@ export function Footer(){
                     <li>LinkedIn</li>
                 </ul>
             </div>
+            <div className="socials">
+                <h3>Social Media</h3>
+                <div className="socials-divider"></div>
+                <ul className="socials-list">
+                    <li>Instagram</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
         </div>
     )
 }
