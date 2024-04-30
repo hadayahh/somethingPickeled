721f3773553f078d1a6e08bd99803cf5c261105f
hadayahh <habaza.dev@gmail.com>
2024-04-30 18:18:55 -0400

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,6 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      {/* <Routes>
-        <Route path="*" element={<App />}></Route>
-        <Route path="/shipping" element={<Shipping />}></Route>
-        <Route path="/nav" element={<Nav />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/footer" element={<Footer />}></Route>
-      </Routes> */}
       <Shipping />
       <Nav />
       <About />
