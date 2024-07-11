86abccb366da1e5d7e9fe4690db7436cad6cb8a7
hadayahh <habaza.dev@gmail.com>
2024-07-11 19:22:02 -0400

    shoppage page additions

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,14 +1,18 @@
 import products from "../data/products.json";
+import Shipping from "../components/Shipping";
+import { Nav } from "../components/Nav";
 
 export function ShopPage() {
   return (
     <>
-      <div>Shop page</div>
+      <Shipping />
+      <Nav />
       {products.map((product) => (
         <li key={product.id}>
           <h2>{product.name}</h2>
           <p>Price: ${product.price}</p>
           <img src={product.imageUrl} alt={product.name} />
+          <h4>Add To Cart</h4>
         </li>
       ))}
     </>
