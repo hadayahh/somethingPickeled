ee42039701bd3178bdd28d1c11a37026fdd9ad7a
hadayahh <habaza.dev@gmail.com>
2024-07-11 19:16:57 -0400

    products render

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,3 +1,16 @@
+import products from "../data/products.json";
+
 export function ShopPage() {
-  return <div>Shop page</div>;
+  return (
+    <>
+      <div>Shop page</div>
+      {products.map((product) => (
+        <li key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Price: ${product.price}</p>
+          <img src={product.imageUrl} alt={product.name} />
+        </li>
+      ))}
+    </>
+  );
 }
