e6bab8903a8b29be886d262132b8e17a39acc93c
hadayahh <habaza.dev@gmail.com>
2024-08-03 21:18:38 -0400

    shopPage type safety

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -3,18 +3,26 @@ import Shipping from "../components/Shipping";
 import { Nav } from "../components/Nav";
 
 export function ShopPage() {
+  type Product = {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+  };
   return (
     <>
       <Shipping />
       <Nav />
-      {products.map((product) => (
-        <li key={product.id}>
-          <h2>{product.name}</h2>
-          <p>Price: ${product.price}</p>
-          <img src={product.imageUrl} alt={product.name} />
-          <h4>Add To Cart</h4>
-        </li>
-      ))}
+      <ul>
+        {products.map((product: Product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>Price: ${product.price}</p>
+            <img src={product.imageUrl} alt={product.name} />
+            <h4>Add To Cart</h4>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
